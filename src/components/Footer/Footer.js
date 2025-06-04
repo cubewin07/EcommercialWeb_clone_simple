150149bc7ea5ab5c717ec19cdca875f6c0e89d92
cubewin <tanthang071208@gmail.com>
2025-06-04 19:54:52 +0700

    Re-desisn footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,20 +1,32 @@
-
 import styles from './Footer.module.scss';
+
 function Footer() {
-    return ( 
-        <footer className={styles.footer}>
-            <div className={styles.footerContent}>
-                <p>&copy; {new Date().getFullYear()} MyShop. All rights reserved.</p>
-                <p>Contact us: <a href="mailto:tanthang071208@gmail.com"> </a> </p>
-                <p>Follow us on social media:</p>
-                <ul className={styles.socialLinks}>
-                    <li><a href="https://www.facebook.com" target="_blank" rel="noopener noreferrer" className={styles.socialLink}>Facebook</a></li>
-                    <li><a href="https://www.twitter.com" target="_blank" rel="noopener noreferrer" className={styles.socialLink}>Twitter</a></li>
-                    <li><a href="https://www.instagram.com" target="_blank" rel="noopener noreferrer" className={styles.socialLink}>Instagram</a></li>
-                </ul>
-            </div>
-        </footer>
-      );
+  return (
+    <footer className={styles.footer}>
+      <div className={styles.footerContent}>
+        
+        <div className={styles.section}>
+          <p>&copy; {new Date().getFullYear()} MyShop.</p>
+          <p>All rights reserved.</p>
+        </div>
+
+        <div className={styles.section}>
+          <p>Contact us:</p>
+          <a href="mailto:tanthang071208@gmail.com">tanthang071208@gmail.com</a>
+        </div>
+
+        <div className={styles.section}>
+          <p>Follow us:</p>
+          <ul className={styles.socialLinks}>
+            <li><a href="https://www.facebook.com" className={styles.socialLink}>Facebook</a></li>
+            <li><a href="https://www.twitter.com" className={styles.socialLink}>Twitter</a></li>
+            <li><a href="https://www.instagram.com" className={styles.socialLink}>Instagram</a></li>
+          </ul>
+        </div>
+        
+      </div>
+    </footer>
+  );
 }
 
 export default Footer;
