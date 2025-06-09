92ff10b1278a8df80204bcb1ab852424623c8334
cubewin <tanthang071208@gmail.com>
2025-06-09 09:33:37 +0700

    Adding empty cart page and change body background

---
+++ src/Pages/Cart/Cart.js
@@ -0,0 +1,19 @@
+import styles from './Cart.module.scss';
+
+function Cart() {
+    return (
+        <div className={styles.cart}>
+            <div className={styles.cartHeader}>
+                <h1 className={styles.cartTitle}>Your Cart</h1>
+                <p className={styles.cartMessage}>Your cart is currently empty.</p>
+            </div>
+            <div className={styles.cartContent}>
+                <img src="/assets/images/empty-cart.png" alt="Empty Cart" className={styles.emptyCartImage} />
+                <p className={styles.cartPrompt}>Browse our products to add items to your cart!</p>
+                <button className={styles.shopButton}>Start Shopping</button>
+            </div>
+        </div>
+    );
+}
+
+export default Cart;
--- src/Routes/routes.js
+++ src/Routes/routes.js
@@ -3,6 +3,7 @@ import { createBrowserRouter } from "react-router-dom";
 import Layout from "../Layout/Layout.js";
 import Home from "../Pages/Home/Home.js";
 import Login from "../Pages/login/login.js";
+import Cart from "../Pages/Cart/Cart.js";
 import NotFound404 from "../Pages/notFound404/NotFound404.js";
 
 export const routes = createBrowserRouter([
@@ -12,6 +13,7 @@ export const routes = createBrowserRouter([
         children: [
             { index: true, element: <Home /> },
             { path: "/login", element: <Login /> },
+            {path: "/cart", element: <Cart /> }
         ]
     },
     {
